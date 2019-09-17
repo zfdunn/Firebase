ba467aa66d27815e5314cfd8a6eda57dc1fd1c57
Zach Dunn <zfdunn@comcast.net>
2019-09-17 09:42:55 -0400

    added missing js components - borrowed classmates code

--- assets/javascript.js
+++ assets/javascript.js
@@ -48,5 +48,42 @@ $(document).ready(function(){
         $("#frequency").val("");
         return false;
     });
+// ---------------------------------------------
+// update list (borrowed code from classmate - start)
+database.orderByChild("dateAdded").on("child_added", function(snapshot){
+    var theTrainTime = snapshot.val().trainTime;
+    var theTrainFrequency = snapshot.val().frequency;
+    console.log(moment(snapshot.val().dateAdded).format("MM/DD/YY"));
+// ---------------------------------------------
+// calculate the next arrival time, and minutes away
+    var firstTimeConverted = moment(theTrainTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm:"));
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERNECE IN TIME: " + diffTime);
+    console.log(frequency);
+    var remainder = diffTime % theTrainFrequency;
+    console.log(remainder);
+    var minutesAway = theTrainFrequency - remainder;
+    console.log("MINUTES UNTIL TRAIN: " + minutesAway);
+    var nextArrival = moment().add(minutesAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm A"));
 
+    var theTrainTime = snapshot.val().trainName;
+    var theDestination = snapshot.val().destination;
+// ---------------------------------------------
+// (borrowed code - end)
+// ref for html to update UI
+    $(".train-name-display").append("<td>", theTrainName);
+    $(".destination-display").append("<td>", theDestination);
+    $(".frequency-display").append("<td>", theTrainFrequency + "minutes");
+    $(".next-arrival-display").append("<td>", moment(nextArrival).format("hh:mm A"));
+    $(".minutes-away-display").append("<td>", minutesAway + "minutes");
+    },
+// ---------------------------------------------
+// handle the errors
+    function(errorObject){
+        console.log("Errors handled: " +errorObject.code);
+    });
 });
